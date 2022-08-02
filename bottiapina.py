14c2466e61a0a7ab45e114c672201c2c7d90fc07
jaamo <jaakko@evermade.fi>
2022-08-02 18:17:18 +0300

    Added info command.

--- bottiapina.py
+++ bottiapina.py
@@ -17,17 +17,8 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 DISCORD_CHANNEL = os.getenv('DISCORD_CHANNEL')
 
-# Bot command prefix. 
+# Bot command prefix.
 bot = commands.Bot(command_prefix='+')
-
-# @bot.command(name='kakatakko')
-# async def nine_nine(ctx):
-#     kakat = [
-#         'joo kakkaa',
-#         'joo älä kakkaa',
-#     ]
-#     response = random.choice(kakat)
-#     await ctx.send(response)
 
 class ApinaCommands(commands.Cog):
     def __init__(self, bot):
@@ -37,9 +28,16 @@ class ApinaCommands(commands.Cog):
     def cog_unload(self):
         self.check_for_new_videos.cancel()
 
-    @commands.command(name="whoareyou")
-    async def onkokakkahata(self, ctx):
-        await ctx.send('I\'m ok!')
+    @commands.command(name="wtf")
+    async def wtf(self, ctx):
+        channel_names = []
+        channels = apinaDB.get_channels()
+        for channel in channels:
+            channel_names.append(channel[1])
+        await ctx.send('''No mitäs mitäs! Olen bottiapina ja lähettelen tänne aina viestiä, kun YouTubeen postaillaan uusia videoita.\n
+Minut on koodattu Pythonilla ja löydyn GitHubista: https://github.com/jaamo/bottiapina\n
+Tällä hetkellä seuraan näitä kanavia: %s.\n
+Kanavalistalle lisäillään Suomalaisia YouTube-kanavia, jotka tuottavat aktiivesti pyöräilyaiheista sisältöä. Jos joku kanava listalta puuttuu, niin vinkkaa ylläpidolle!''' % (", ".join(channel_names)))
 
     @tasks.loop(seconds = 900) # 15 mins
     async def check_for_new_videos(self):
@@ -52,13 +50,13 @@ class ApinaCommands(commands.Cog):
             for video in new_videos:
                 await channel.send("Uusi video! %s: %s %s" % (video["channel_name"], video["video_title"], video["video_url"]))
                 apinaDB.update_latest_video(
-                    video["channel_id"], 
+                    video["channel_id"],
                     video["video_id"],
                     video["video_title"],
                     video["video_url"],
                     video["video_description"],
                 )
         else:
-            print("Connection to Discord is down.")
+            print("Connection to Discord is down. Retrying soon...")
 bot.add_cog(ApinaCommands(bot))
 bot.run(TOKEN)
